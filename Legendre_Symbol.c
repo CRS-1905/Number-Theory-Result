78f80a7e2f130df1256cf1ade5bc3edf39073442
CRS-1905 <62536848+CRS-1905@users.noreply.github.com>
2020-04-07 03:24:10 +0530

    Update Legendre_Symbol.c

--- Legendre_Symbol.c
+++ Legendre_Symbol.c
@@ -1,5 +1,28 @@
 #include<stdio.h>
 
+int gcd(int a,int b)
+{
+	int r0,r1,q,t;
+	if(a==0)
+		return b;
+	if(b==0)
+		return a;
+	r0=a;
+	if(r0<0)
+		r0=-r0;
+	r1=b;
+	if(r1<0)
+		r1=-r1;
+	while(r1>0)
+	{
+		q=r0/r1;
+		t=r0-(r1*q);
+		r0=r1;
+		r1=t;
+	}
+	return r0;
+}
+
 int power(int i)
 {
 	if(i%2==0)
@@ -17,8 +40,6 @@ int legendra_symbol(int a,int p)
 			a=a+p;
 	}
 	a1=a%p;
-	if(a1==0)
-		return 0;
 	if(a1==1)
 		return 1;
 	if(a1==p-1)
@@ -30,21 +51,8 @@ int legendra_symbol(int a,int p)
 		else
 			return power((p+1)/4);
 	}
-	s=1;
-	for(i=2;i<a1;i++)
-	{
-		if(a1%i==0)
-		{
-			s=0;
-			return legendra_symbol(i,p)*legendra_symbol(a1/i,p);
-			break;
-		}
-	}
-	if(s==1)
-	{
-		r=power(((a1-1)/2)*((p-1)/2));
-		return r*legendra_symbol(p,a1);
-	}
+	r=power(((a1-1)/2)*((p-1)/2));
+	return r*legendra_symbol(p,a1);
 }
 
 int main()
@@ -54,8 +62,14 @@ int main()
 	scanf("%d",&p);
 	printf("Enter An Integer :\t");
 	scanf("%d",&a);
-	d=legendra_symbol(a,p);
-	if(d==p-1)
-		d=-1;
+	if(gcd(a,p)>1)
+		d=0;
+	else
+	{
+		d=legendra_symbol(a,p);
+		if(d==p-1)
+			d=-1;
+	}
+	printf("%d\n",gcd(a,p));
 	printf("The Legendre Symbol (%d,%d) =%d\n",a,p,d);
 }
